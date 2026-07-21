9a343d43e067b87d555afda606fe5d75c250daf3
Harshit-2118 <70027602+Harshit-2118@users.noreply.github.com>
2026-07-21 09:33:40 +0530

    Create Task3.js

---
+++ Set2/Task3.js
@@ -0,0 +1,8 @@
+// Task 3: Hospital Patient System — Identify Datatypes
+let patientAge = 45;           // Number
+let patientName = "John Doe";  // String
+let isAdmitted = true;         // Boolean
+
+console.log("patientAge type:", typeof patientAge);     // "number"
+console.log("patientName type:", typeof patientName);   // "string"
+console.log("isAdmitted type:", typeof isAdmitted);     // "boolean"
