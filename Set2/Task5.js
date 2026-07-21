a286db49982b36fbe46b3a4f8ff05ddf0733ff64
Harshit-2118 <70027602+Harshit-2118@users.noreply.github.com>
2026-07-21 09:34:14 +0530

    Create Task5.js

---
+++ Set2/Task5.js
@@ -0,0 +1,10 @@
+// Task 5: Hospital Patient System — Classify a Record
+let patientAge = 65;
+
+if (patientAge >= 60) {
+    console.log("Patient is a Senior Citizen.");
+} else if (patientAge >= 18) {
+    console.log("Patient is an Adult.");
+} else {
+    console.log("Patient is a Minor.");
+}
